d867778f5fd658a36cbf73fee72f76e919fa174d
Alan Leonardo Flores <aflores@accionpoint.com>
2025-10-17 15:23:45 -0300

    Estructura final: proyectos separados de Cypress y Selenium con Jenkinsfiles independientes

---
+++ automatizaciones/selenium_project/selenium_local/helpers/__init__.py
@@ -0,0 +1,11 @@
+"""
+Helpers para automatizaciones GeneXus con Selenium.
+"""
+
+from .gx_helpers import (
+    completar_fecha_nacimiento,
+    completar_fecha_nacimiento_click,
+    esperar_y_setear_combo,
+    nivel_educativo_random,
+    set_gx_select,
+)
--- selenium_local/helpers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Helpers para automatizaciones GeneXus con Selenium.
-"""
-
-from .gx_helpers import (set_gx_select, esperar_y_setear_combo, nivel_educativo_random, completar_fecha_nacimiento, completar_fecha_nacimiento_click)
-
-
-
-
